2f104fec583e3cd0cc2a6f8080c42495c70d2b1d
JohannHolm <johannholm94@gmx.de>
2023-10-09 13:54:11 +0200

    JS ABOUT Section on Open Card

--- about.js
+++ about.js
@@ -1,30 +1,29 @@
-function returnPokemonAbout() {
+function returnPokemonAboutHTML() {
   return `
 <table class="table">
     <tr>
         <td>Description</td>
-        <td>Charakteristik</td><!--https://pokeapi.co/api/v2/characteristic/{id}/-->
+        <td id="pokemon-character">loading...</td><!--https://pokeapi.co/api/v2/characteristic/{id}/-->
     </tr>
     <tr>
         <td>Height</td>
-        <td>inch und cm</td>
+        <td id="pokemon-height">loading...</td>
     </tr>
     <tr>
         <td>Weight</td>
-        <td>lbs und kg</td>
+        <td id="pokemon-weight">loading...</td>
     </tr>
 
-    <tr><td class="about-section-2-headline h5">Breeding</td></tr>
+    <!--<tr><td class="about-section-2-headline h5">Breeding</td></tr>-->
 
     <tr>
-        <td>Gender</td><!--https://pokeapi.co/api/v2/gender/{id or name}/-->
-        <td>Geschlecht</td>
+        <td>Best Stat</td>
+        <td id="pokemon-best-stat">loading...</td>
     </tr>
     <tr>
-        <td>Egg Groups</td>
-        <td>string</td><!--https://pokeapi.co/api/v2/egg-group/{id or name}/-->
+        <td>Habitat</td>
+        <td id="pokemon-habitat">loading...</td><!--https://pokeapi.co/api/v2/pokemon-species/{id or name}/ ##habitat - name##-->
     </tr>
 </table>
 `;
 }
-// Pokemon-Typ 'https://pokeapi.co/api/v2/pokemon-species/6/' genera => 7 => genus
--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let currentPokemon;
+let currentPokemonDescription;
+let currentPokemonHabitat;
+let currentPokemonBestStat;
 let CARD_LIMIT = []; //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
 let loadMore;
 const BACKGROUND_COLORS = {
@@ -31,7 +34,7 @@ function calcCardLimit() {
 async function init() {
   //load first 20. Pokemons
   calcCardLimit();
-  getJsonFromLocalStorage();
+  //getJsonFromLocalStorage();
   for (let i = 0; i < CARD_LIMIT.length; i++) {
     const id = CARD_LIMIT[i];
     let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -57,18 +60,56 @@ function getJsonFromLocalStorage(id) {
 function openPokemon(id) {
   document.getElementById('card-box').classList.remove('d-none');
   document.getElementById('card-box-wrapper').classList.remove('d-none');
-  renderPokemonInfo(id);
+  renderPokemonInfoTopSection(id);
 }
 
-function renderPokemonInfo(id) {
+function renderPokemonInfoTopSection(id) {
   // große Detail Karte
   document.getElementById('card-box').innerHTML = returnPokemonCard();
   document.getElementById('pokemon-name').innerHTML = capitalizeFirstLetter(getJsonFromLocalStorage(id)['name']);
   document.getElementById('pokemon-image').src = getJsonFromLocalStorage(id)['sprites']['other']['home']['front_default'];
   document.getElementById('pokemon-id').innerHTML = `#${addIdFormat(getJsonFromLocalStorage(id)['id'])} `;
   renderPokemonTypesCard(getJsonFromLocalStorage(id));
-  renderAboutPokemon();
+  renderAboutPokemonHTML(id);
   setBackgroundColorOpenCard(getJsonFromLocalStorage(id), 'main-card');
+}
+
+async function renderAboutPokemonHTML(id) {
+  document.getElementById('pokemon-info-box').innerHTML = returnPokemonAboutHTML();
+  await fetchPokemonCharacter(id);
+  await fetchPokemonHabitat(id);
+  document.getElementById('pokemon-character').innerHTML = currentPokemonDescription;
+  document.getElementById('pokemon-height').innerHTML = getPokemonHeightFromLS(id) + `cm`;
+  document.getElementById('pokemon-weight').innerHTML = getPokemonWeightFromLS(id) + `kg`;
+  document.getElementById('pokemon-habitat').innerHTML = capitalizeFirstLetter(currentPokemonHabitat);
+  document.getElementById('pokemon-best-stat').innerHTML = capitalizeFirstLetter(currentPokemonBestStat);
+}
+
+function getPokemonHeightFromLS(id) {
+  let pokemon = getJsonFromLocalStorage(id);
+  let pokemonHeightInCm = pokemon['height'] * 10;
+  return pokemonHeightInCm;
+}
+
+function getPokemonWeightFromLS(id) {
+  let pokemon = getJsonFromLocalStorage(id);
+  let pokemonWeightInKg = Math.round(pokemon['weight'] / 10); // Hectogram to kg
+  return pokemonWeightInKg;
+}
+
+async function fetchPokemonCharacter(id) {
+  let url = `https://pokeapi.co/api/v2/characteristic/${id}`;
+  let response = await fetch(url);
+  let responseAsJson = await response.json();
+  currentPokemonDescription = responseAsJson['descriptions']['7']['description'];
+  currentPokemonBestStat = responseAsJson['highest_stat']['name'];
+}
+
+async function fetchPokemonHabitat(id) {
+  let url = `https://pokeapi.co/api/v2/pokemon-species/${id}`;
+  let response = await fetch(url);
+  let responseAsJson = await response.json();
+  currentPokemonHabitat = responseAsJson['habitat']['name'];
 }
 
 function closeCard() {
@@ -89,11 +130,6 @@ function capitalizeFirstLetter(string) {
 function addIdFormat(id) {
   // ID soll immer im Format 000 dargestellt werden.
   return (prepended_number = String(id).padStart(3, '0'));
-}
-
-function renderAboutPokemon() {
-  document.getElementById('pokemon-info-box').innerHTML = returnPokemonAbout();
-  // return Charakteristik Funktion in js Template
 }
 
 function renderPokemonTypes(string, id) {
